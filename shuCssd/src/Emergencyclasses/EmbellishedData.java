b76e99b52f3ebb904979c7d469f2781f0bd8f844
akila95 <akilaf95@gmail.com>
2018-07-14 10:09:01 +0530

    Emergency Classes

---
+++ shuCssd/src/Emergencyclasses/EmbellishedData.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Emergencyclasses;
+
+/**
+ *
+ * @author Piyu
+ */
+public class EmbellishedData {
+   
+    public Long TimeInMiles;
+     SensorMonitor monitorSet;
+    public String sensorID;
+//        private List<SensorMonitor> observers ;
+    
+    public EmbellishedData(SensorMonitor sensorMonitor) {
+       
+        
+         for (Sensor ob : sensorMonitor) {
+//                      System.out.println("Notifying all the subscribers when the bin is full oi");
+                     System.out.println("wadda EmbellishedData");
+                     
+                 }
+                   
+    }
+ 
+}
